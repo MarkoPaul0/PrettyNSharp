6bb464b9dd3c7b90e561ee319ba2f6ce6af85455
Markus Leballeux <markus.leballeux@gmail.com>
2018-04-21 14:31:50 -0500

    Set up other dependency properties to set colors on hover as well as height and width of vector.

--- SharpDisplay.xaml.cs
+++ SharpDisplay.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,6 +31,22 @@ namespace WPFSharpener
         public Brush VectorBrush { get { return (Brush)GetValue(VectorBrushProperty); } set { SetValue(VectorBrushProperty, value); } }
         public static readonly DependencyProperty VectorBrushProperty =
             DependencyProperty.Register("VectorBrush", typeof(Brush), typeof(SharpDisplay), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+        public Brush HighlightBrush { get { return (Brush)GetValue(HighlightBrushProperty); } set { SetValue(HighlightBrushProperty, value); } }
+        public static readonly DependencyProperty HighlightBrushProperty =
+            DependencyProperty.Register("HighlightBrush", typeof(Brush), typeof(SharpDisplay), new FrameworkPropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+        public double VectorWidth { get { return (double)GetValue(VectorWidthProperty); } set { SetValue(VectorWidthProperty, value); } }
+        public static readonly DependencyProperty VectorWidthProperty =
+            DependencyProperty.Register("VectorWidth", typeof(double), typeof(SharpDisplay), new FrameworkPropertyMetadata(default(double)));
+
+        public double VectorHeight { get { return (double)GetValue(VectorHeightProperty); } set { SetValue(VectorHeightProperty, value); } }
+        public static readonly DependencyProperty VectorHeightProperty =
+            DependencyProperty.Register("VectorHeight", typeof(double), typeof(SharpDisplay), new FrameworkPropertyMetadata(default(double)));
+
+        public Brush BackgroundOnHover { get { return (Brush)GetValue(BackgroundOnHoverProperty); } set { SetValue(BackgroundOnHoverProperty, value); } }
+        public static readonly DependencyProperty BackgroundOnHoverProperty =
+            DependencyProperty.Register("BackgroundOnHover", typeof(Brush), typeof(SharpDisplay), new FrameworkPropertyMetadata(new SolidColorBrush(Colors.LightGreen)));
         #endregion
 
 
@@ -37,13 +55,22 @@ namespace WPFSharpener
             InitializeComponent();
             this.Loaded += (sender, args) =>
             {
-                if(this.Vector == null)
+                if (this.Vector == null)
                 {
                     this.Vector = Constants.DEFAULT_PATH;
                 }
             };
         }
 
-        
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+        #endregion
     }
 }
